154692c7016af9f2ce0538cb42b7a8280e67b037
PVT1701 <pvtzzz1701@gmail.com>
2025-09-18 16:43:25 +0700

    .

--- no1/src/App.js
+++ no1/src/App.js
@@ -1,12 +1,36 @@
-import { Button} from 'antd';
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './redux/slides/counterSlide'
+import styled from 'styled-components';
 
-function App() {
+export function App() {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
+  const Button = styled.button({
+    color: 'red',
+    background: 'black',
+  });
+
   return (
     <div>
-      Hello Word!
-      <Button type="primary">Primary Button</Button>
+      <div>
+        <Button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </Button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
